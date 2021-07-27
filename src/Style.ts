da62cfa3322e6da292bb8969604458440fe7452e
morshedalam3 <morshedhasan1969@gmail.com>
2021-07-28 00:37:13 +0600

    add all

---
+++ src/Style.ts
@@ -0,0 +1,60 @@
+import styled, { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+  html {
+    height: 100%;
+  }
+
+  body {
+    background-color: black;
+    background-size: cover;
+    margin: 0;
+    padding: 0 20px;
+    display: flex;
+    justify-content: center;
+  }
+
+  * {
+    font-family: 'Catamaran', sans-serif;
+    box-sizing: border-box;
+  }
+`;
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  > p {
+    color: #fff;
+  }
+
+  .score {
+    color: #fff;
+    font-size: 2rem;
+    margin: 0;
+  }
+
+  h1 {
+    font-family: Fascinate;
+    font-size: 70px;
+    text-align: center;
+    margin: 20px;
+    color:white;
+  }
+
+  .start, .next {
+    cursor: pointer;
+    background: white;
+    border: 2px solid #d38558;
+    box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.25);
+    border-radius: 10px;
+    height: 40px;
+    margin: 20px 0;
+    padding: 0 40px;
+  }
+
+  .start {
+    max-width: 200px;
+  }
+`;
